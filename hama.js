b3b5dc1fa96a6af64206cfb017daccdc1de95441
DevHAMO <96010555+DevHAMO@users.noreply.github.com>
2021-12-14 04:34:16 -0800

    Create hama.js

---
+++ hama.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const Carlo = new Discord.Client();
+Hamaa.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`SHTEK BO STREAMING AKA BNUSA`,];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    Hamaa.user.setActivity(STREAMING, {      
+    type: "STREAMING", 
+    url: "https://www.twitch.tv/faith"
+    });
+  }, 2000);
+});
+Hamaa.on("ready", () => {
+    var join = Carlo.channels.get(""); ///id Voice
+    if (join) join.join();
+  });
+Hamaa.login(""); ///Token
